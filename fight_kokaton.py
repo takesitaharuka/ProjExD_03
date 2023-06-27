6339c6435ee89f54b7de7df31f33a5e1c297b4f7
takesitaharuka <c0a22096ac@edu.teu.ac.jp>
2023-06-27 15:45:46 +0900

    練習2.

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -131,7 +131,7 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load("ex03/fig/pg_bg.jpg")
     bird = Bird(3, (900, 400))
-    bomb = Bomb((255, 0, 0), 100)
+    bomb = Bomb((255, 0, 0), 10)
     beam = None
 
     clock = pg.time.Clock()
@@ -158,6 +158,8 @@ def main():
             if beam.rct.colliderect(bomb.rct):
                 bomb = None
                 beam = None
+                bird.change_img(6, screen)
+                pg.display.update()
                            
 
         key_lst = pg.key.get_pressed()
